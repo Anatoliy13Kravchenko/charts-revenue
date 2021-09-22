ed4c58bcb6762eb978706d1a3a0637baba8b1692
Anatolii <anatoliy3787@gmail.com>
2021-09-22 16:01:29 +0300

    Driver

--- src/View/Charts/index.tsx
+++ src/View/Charts/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import O2O from "./O2O";
+import Driver from "./Driver";
 
 interface Props {
   activeChart: string;
@@ -8,6 +9,12 @@ const Charts = ({ activeChart }: Props) => {
   const renderChart = (chart: string) => {
     if (chart === "O2O revenue breakdown") {
       return <O2O />;
+    }
+    if (chart === "Revenue drivers") {
+      return <Driver />;
+    }
+    if (chart === "Marketing performance") {
+      return <Driver />;
     }
     return "";
   };
